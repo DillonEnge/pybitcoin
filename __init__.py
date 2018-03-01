ba4cf535abc70202ac03aad6fb9af8f6da236076
Dillon Enge <dillon.enge@gmail.com>
2018-03-01 02:16:02 -0500

    Fixed structure.

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .addressFetcher import AddressFetcher
--- addressFetcher.py
+++ addressFetcher.py
@@ -6,32 +6,21 @@ class GarlicoinPrivateKey(BitcoinPrivateKey):
 class LitecoinPrivateKey(BitcoinPrivateKey):
     _pubkeyhash_version_byte = 48
 
-def generateGarlicAddress(private = False):
-    private_key = BitcoinPrivateKey()
-    public_key = private_key.public_key()
-    garlicoin_private_key = GarlicoinPrivateKey(private_key.to_hex())
-    garlicoin_public_key = garlicoin_private_key.public_key()
-    f = open('../addresses.txt','a')
-    if private:
-        f.write('\nPublic, private key: ' + str([garlicoin_public_key.address(), garlicoin_private_key.to_hex()]))
-        f.close()  
-        return [garlicoin_public_key.address(), garlicoin_private_key.to_hex()]
-    f.write('\nPublic key: ' + str(garlicoin_public_key.address()))
-    f.close()
-    return garlicoin_public_key.address()
+class AddressFetcher():
+    def generateGarlicAddress(private = False):
+        private_key = BitcoinPrivateKey()
+        public_key = private_key.public_key()
+        garlicoin_private_key = GarlicoinPrivateKey(private_key.to_hex())
+        garlicoin_public_key = garlicoin_private_key.public_key()
+        if private:  
+            return [garlicoin_public_key.address(), garlicoin_private_key.to_hex()]
+        return garlicoin_public_key.address()
 
-def generateLiteAddress(private = False):
-    private_key = BitcoinPrivateKey()
-    public_key = private_key.public_key()
-    litecoin_private_key = LitecoinPrivateKey(private_key.to_hex())
-    litecoin_public_key = litecoin_private_key.public_key()
-    f = open('../addresses.txt','a')
-    if private:
-        f.write('\nPublic, private key: ' + str([litecoin_public_key.address(), litecoin_private_key.to_hex()]))
-        f.close()  
-        return [litecoin_public_key.address(), litecoin_private_key.to_hex()]
-    f.write('\nPublic key: ' + str(litecoin_public_key.address()))
-    f.close()
-    return litecoin_public_key.address()
-
-generateGarlicAddress()
+    def generateLiteAddress(private = False):
+        private_key = BitcoinPrivateKey()
+        public_key = private_key.public_key()
+        litecoin_private_key = LitecoinPrivateKey(private_key.to_hex())
+        litecoin_public_key = litecoin_private_key.public_key()
+        if private: 
+            return [litecoin_public_key.address(), litecoin_private_key.to_hex()]
+        return litecoin_public_key.address()
